43b2baeddbc5a06e6f13141aa08d69cc4df31b7c
Satvik Upadhyaya <2405757@kiit.ac.in>
2025-07-24 00:23:49 +0530

    Merge upstream/main into issue-branch-1 and some more issues

--- lib/firebase.tsx
+++ lib/firebase.tsx
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth'; // Import Auth if needed
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBB7yE5iP38-103sPc3uxQjVSwLgWqPR7g",
-  authDomain: "fail-u-forward-b4f39.firebaseapp.com",
-  projectId: "fail-u-forward-b4f39",
-  storageBucket: "fail-u-forward-b4f39.firebasestorage.app",
-  messagingSenderId: "280044895991",
-  appId: "1:280044895991:web:c35e0aca6ef4264c985df2",
-  measurementId: "G-THEWTCCHGS"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "1",
+  measurementId: ""
 };
 
 // Initialize Firebase
