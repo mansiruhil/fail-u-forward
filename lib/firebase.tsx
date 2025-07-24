9f1bca531dad00d9ad63cc582792b82e7ca9afe5
Karan <karansharma192004@gmail.com>
2025-07-24 12:50:13 +0530

    Using env in lib/firebase

--- lib/firebase.tsx
+++ lib/firebase.tsx
@@ -4,15 +4,14 @@ import { getAuth } from 'firebase/auth'; // Import Auth if needed
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "1",
-  measurementId: ""
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MESUREMENT_ID || ""
 };
-
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
