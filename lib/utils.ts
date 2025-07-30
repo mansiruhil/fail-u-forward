6d27647bcfbfd141b5901bdecf1709b86050ec32
mansi ruhil <157950124+mansiruhil@users.noreply.github.com>
2025-07-30 23:39:57 +0530

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,23 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { firebaseApp } from "@/lib/firebase";
 
+// For merging Tailwind classes
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+// For checking username availability in Firestore
+const db = getFirestore(firebaseApp);
+
+export async function checkUsername(username: string) {
+  if (username === "") return false;
+
+  const usersRef = collection(db, "users");
+  const querySnapshot = await getDocs(usersRef);
+
+  const user = querySnapshot.docs.find(doc => doc.data().username === username);
+
+  return !user;
 }
