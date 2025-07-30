10db089014e7f7ea2b44c020d82b0f610912f4e0
mansi ruhil <157950124+mansiruhil@users.noreply.github.com>
2025-07-30 23:47:53 +0530

    Update route.ts

--- app/api/check-username/route.ts
+++ app/api/check-username/route.ts
@@ -1,12 +1,10 @@
 import { NextResponse } from "next/server";
-import { checkUsername } from "@/lib/utils";
+import { checkUsername } from "@/lib/db/queries"; // instead of utils
 
 export async function POST(req: Request) {
   try {
     const { username } = await req.json();
-
     const isAvailable = await checkUsername(username);
-
     return NextResponse.json({ available: isAvailable });
   } catch (err) {
     console.error("Username check failed:", err);
