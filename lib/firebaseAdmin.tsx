467734a5cfb0ed8da5a57b8a48c7e532e4c3162f
mansi ruhil <157950124+mansiruhil@users.noreply.github.com>
2025-08-03 20:22:10 +0530

    Update firebaseAdmin.tsx

--- lib/firebaseAdmin.tsx
+++ lib/firebaseAdmin.tsx
@@ -1,6 +1,10 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
+  if (!process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
+    throw new Error("Missing FIREBASE_SERVICE_ACCOUNT_BASE64 env variable");
+  }
+
   const serviceAccount = JSON.parse(
     Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, "base64").toString("utf8")
   );
