31101bd9ff41963afebebbda061379795371038f
mansi ruhil <157950124+mansiruhil@users.noreply.github.com>
2025-08-06 14:48:32 +0530

    Update and rename tailwind.config.js to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
   darkMode: "class",
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -62,12 +63,20 @@ module.exports = {
       },
       keyframes: {
         'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
+          from: {
+            height: '0',
+          },
+          to: {
+            height: 'var(--radix-accordion-content-height)',
+          },
         },
         'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
+          from: {
+            height: 'var(--radix-accordion-content-height)',
+          },
+          to: {
+            height: '0',
+          },
         },
       },
       animation: {
@@ -78,3 +87,4 @@ module.exports = {
   },
   plugins: [require('tailwindcss-animate')],
 };
+export default config;
