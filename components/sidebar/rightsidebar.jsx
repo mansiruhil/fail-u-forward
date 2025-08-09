5c62538d30ea2134d8e17f29389280f985293a41
Karan <karansharma192004@gmail.com>
2025-08-09 10:31:34 +0530

    Removed random counts from sidebar

--- components/sidebar/rightsidebar.jsx
+++ components/sidebar/rightsidebar.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import Link from "next/link";
-import { BookmarkX, BookOpenText, TrendingUp } from "lucide-react";
+import { ArrowRight, BookmarkX, BookOpenText, TrendingUp } from "lucide-react";
 
 const TRENDING_TOPICS = [
-  { id: 1, title: "Startup Fails Stories", count: "1,234", icon: <TrendingUp className="w-5 h-5"/>},
-  { id: 2, title: "Interview Rejections", count: "956", icon: <BookmarkX className="w-5 h-5"/> },
-  { id: 3, title: "Project U-Turns", count: "847", icon: <BookOpenText className="w-5 h-5"/> },
+  { id: 1, title: "Startup Fails Stories", icon: <TrendingUp className="w-5 h-5" /> },
+  { id: 2, title: "Interview Rejections", icon: <BookmarkX className="w-5 h-5" /> },
+  { id: 3, title: "Project U-Turns", icon: <BookOpenText className="w-5 h-5" /> },
 ];
 
 const FAILURE_CATEGORIES = [
-  { id: 1, title: "Tech Interviews", count: 12},
-  { id: 2, title: "Pitch Meetings", count: 33},
-  { id: 3, title: "Product Launches", count: 7},
-  { id: 4, title: "Startup Failures", count: 21},
-  { id: 5, title: "Funding Rejections", count: 8},
-  { id: 6, title: "Team Conflicts", count: 41},
-  { id: 7, title: "Marketing Missteps", count: 20}
+  { id: 1, title: "Tech Interviews" },
+  { id: 2, title: "Pitch Meetings" },
+  { id: 3, title: "Product Launches" },
+  { id: 4, title: "Startup Failures" },
+  { id: 5, title: "Funding Rejections" },
+  { id: 6, title: "Team Conflicts" },
+  { id: 7, title: "Marketing Missteps" }
 ]
 
 export function RightSidebar() {
@@ -28,13 +28,18 @@ export function RightSidebar() {
             <Link
               href={`/topic/${topic.id}`}
               key={topic.id}
-              className="block p-3 border border-border bg-card rounded-lg hover:bg-muted transition-colors"
+              className="group block p-3 border border-border bg-card rounded-lg hover:bg-muted transition-colors"
             >
-              <div className="flex gap-1">
-              {topic.icon}
-              <p className="text-sm font-medium text-foreground">{topic.title}</p>
+              <div className="flex justify-between items-center">
+                <div className="flex gap-1 items-center">
+                  {topic.icon}
+                  <p className="text-sm font-medium text-foreground">{topic.title}</p>
+                </div>
+
+                <ArrowRight
+                  className="w-5 h-5 transform transition-transform duration-600 group-hover:-rotate-25 "
+                />
               </div>
-              <p className="text-xs text-muted-foreground">{topic.count} people sharing</p>
             </Link>
           ))}
         </div>
@@ -50,7 +55,6 @@ export function RightSidebar() {
               className="relative p-2 bg-background text-foreground border border-gray-400 hover:border-black  rounded-lg transition-all duration-100"
             >
               <span className="text-sm text-foreground">{category.title}</span>
-              <span className="absolute -top-2 -right-1 rounded-full text-white text-xs bg-neutral-800 flex justify-center items-center w-5 h-5 z-10">{category.count}</span>
             </Link>
           ))}
         </div>
